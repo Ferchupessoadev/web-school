3343cbafe8dd6ecf256cd421aa38428f73f82398
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-09-19 06:36:54 -0300

    welcome section changed

--- resources/views/partials/welcome.php
+++ resources/views/partials/welcome.php
@@ -1,6 +1,33 @@
 <section class="section-school">
 	<div class="container-text">
-		<p>Bienvenidos</p>
-		<h1>Técnica N°2 "Independencia"</h1>
+		<h1>E.E.T N°2 "INDEPENDENCIA"</h1>
+		<p class="section-school__text">La Escuela Técnica N° 2 "Independencia" de Concordia forma a sus estudiantes mediante talleres prácticos y una fuerte vinculación con el mundo laboral, preparándolos para enfrentar desafíos profesionales y académicos.</p>
+		<div class="section-school__links">
+			<a 
+				class="section-school__link"
+				target="_blank"
+				href="https://www.facebook.com/profile.php?id=100064268132995"
+			>
+				<i class="fa-brands fa-facebook"></i>
+			</a>
+			<a 
+				class="section-school__link"
+				target="_blank"
+				href="https://www.youtube.com/@tecnican2independencia382"
+			>
+				<i class="fa-brands fa-youtube"></i>
+			</a>
+		</div>
+		<div class="section-school__info">
+			<div class="section-school__info-text">
+				<i class="fa-solid fa-location-dot"></i>
+				<p>Alberdi 223</p>
+			</div>
+			<div class="section-school__info-text">
+				<i class="fa-solid fa-phone"></i>
+				<p>+57 315 123 4567</p>
+			</div>
+		</div>
 	</div>
+	<img class="section-school__img" src="/assets/img/banner.jpg" alt="puerta principal de la tecnica 2 independencia">
 </section>
